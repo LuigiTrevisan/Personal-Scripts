d6e07333f1d75301fc4216cf7856c7e6c0b835a9
Luigi Trevisan <checkzin123@gmail.com>
2024-01-14 00:04:40 -0300

    fixes on main loop and order of attributes

--- Python/WWESupercard-Codebreaker-Solver/codebreaker.py
+++ Python/WWESupercard-Codebreaker-Solver/codebreaker.py
@@ -4,6 +4,8 @@ from colorama import Fore, Style, Back
 colors = ['▲', '▼', '◄', '►']
 num_pegs = 4
 unknown = '?'
+all_combinations = list(itertools.product(colors, repeat=num_pegs))
+current_guess = ('▲', '▲', '▼', '▼')
 
 
 def score_guess(guess, all_combinations):
@@ -27,12 +29,14 @@ def get_best_guess(all_combinations):
             best_guess = guess
     return best_guess
 
+
 def next_guess(all_combinations):
     min_max = float('inf')
     for guess in all_combinations:
         max_combinations = 0
         for feedback in itertools.product(range(num_pegs + 1), repeat=2):
-            count = len([comb for comb in all_combinations if get_feedback(guess, comb) == feedback])
+            count = len(
+                [comb for comb in all_combinations if get_feedback(guess, comb) == feedback])
             max_combinations = max(max_combinations, count)
         if max_combinations < min_max:
             min_max = max_combinations
@@ -50,11 +54,6 @@ def print_guess(guess, no_score=False):
         print(Back.RESET + Fore.RESET + "\nThe score of this guess is: " + Fore.RED +
               str(score_guess(current_guess, all_combinations)))
     print(Style.RESET_ALL)
-
-
-all_combinations = list(itertools.product(colors, repeat=num_pegs))
-
-current_guess = ('▲', '▲', '▼', '▼')
 
 
 def get_feedback(guess, secret):
@@ -80,15 +79,21 @@ def reset_game(error=None):
     return all_combinations, current_guess
 
 
-print(Back.RED + Fore.BLACK + "Welcome to Codebreaker Solver!" + Style.RESET_ALL)
-print(Back.GREEN + Fore.BLACK + "Developed by: @LuigiTrevisan\n" + Style.RESET_ALL)
-
-while True:
-    print_guess(current_guess)
+def get_user_feedback():
     black = input(Fore.GREEN + "✅: " + Style.RESET_ALL)
+    while black not in [str(i) for i in range(num_pegs + 1)] or not (black.isdigit() or black == unknown):
+        black = input(Fore.GREEN + "✅: " + Style.RESET_ALL)
     black = int(black) if black != unknown else unknown
     white = input(Fore.GREEN + "⚪: " + Style.RESET_ALL)
+    while white not in [str(i) for i in range(num_pegs + 1)] or not (white.isdigit() or white == unknown):
+        white = input(Fore.GREEN + "⚪: " + Style.RESET_ALL)
     white = int(white) if white != unknown else unknown
+    return black, white
+
+
+def game_loop(all_combinations, current_guess):
+    print_guess(current_guess)
+    black, white = get_user_feedback()
     print()
 
     if black == num_pegs:
@@ -97,13 +102,14 @@ while True:
         print_guess(current_guess, no_score=True)
         print(Back.RESET + Fore.RESET + "🌟🌟🌟\n" + Style.RESET_ALL)
         all_combinations, current_guess = reset_game()
-        continue
+        return all_combinations, current_guess
 
-    all_combinations = filter_combinations(all_combinations, current_guess, (black, white))
+    all_combinations = filter_combinations(
+        all_combinations, current_guess, (black, white))
     if len(all_combinations) < 1:
         all_combinations, current_guess = reset_game(
             "There are no combinations that match that feedback. Please check your input and try again.")
-        continue
+        return all_combinations, current_guess
 
     if len(all_combinations) == 1:
         print(Back.RESET + Fore.RESET + "🌟🌟🌟" + Style.RESET_ALL)
@@ -111,6 +117,25 @@ while True:
         print_guess(all_combinations[0], no_score=True)
         print(Back.RESET + Fore.RESET + "🌟🌟🌟\n" + Style.RESET_ALL)
         all_combinations, current_guess = reset_game()
-        continue
+        return all_combinations, current_guess
 
     current_guess = next_guess(all_combinations)
+    return all_combinations, current_guess
+
+
+if __name__ == "__main__":
+    print(Back.RED + Fore.BLACK + "Welcome to Codebreaker Solver!" + Style.RESET_ALL)
+    print(Back.GREEN + Fore.BLACK +
+          "Developed by: @LuigiTrevisan\n" + Style.RESET_ALL)
+
+    while True:
+        try:
+            all_combinations, current_guess = game_loop(
+                all_combinations, current_guess)
+        except KeyboardInterrupt:
+            print(Back.RESET + Fore.RED + "\n\nExiting..." + Style.RESET_ALL)
+            break
+        except Exception as e:
+            print(Back.RESET + Fore.RED + "\n\nAn error occured: \n" +
+                  str(e) + Style.RESET_ALL)
+            break
